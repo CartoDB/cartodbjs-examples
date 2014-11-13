e46a123754444147f518d0f3b09860da4c6094b2
xavijam <xavijam@gmail.com>
2014-11-13 18:24:31 +0100

    please, finally working

--- force-gmaps/demo.js
+++ force-gmaps/demo.js
@@ -1,11 +1,13 @@
-$(document).ready(function() {   
-  var vizjson_url = "http://team.cartodb.com/api/v2/viz/9f130ef2-6371-11e4-a0fd-0e9d821ea90d/viz.json";    
-  cartodb.createVis("map", vizjson_url, {
-    share: true,
-    title: true,
-    description: true,
-    search: true,
-    gmaps_base_type: 'roadmap',
-    gmaps_style: '[{"featureType": "water","elementType": "geometry","stylers": [{ "color": "#80a580" }]}]'
+window.onload = function() {
+  cdb.init(function() {
+    var vizjson_url = "http://team.cartodb.com/api/v2/viz/9f130ef2-6371-11e4-a0fd-0e9d821ea90d/viz.json";    
+    cartodb.createVis("map", vizjson_url, {
+      share: true,
+      title: true,
+      description: true,
+      search: true,
+      gmaps_base_type: 'roadmap',
+      gmaps_style: '[{"featureType": "water","elementType": "geometry","stylers": [{ "color": "#80a580" }]}]'
+    });
   });
-});
+}
